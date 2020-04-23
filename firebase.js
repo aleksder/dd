b09a0770d533bad0ebf66de451c74dae66c524e8
aleksder <39030891+aleksder@users.noreply.github.com>
2020-04-23 13:01:20 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyDljOtybsg7pTBI6Pse4kyuGtiHD2eGEEg",
+    authDomain: "webapp-59892.firebaseapp.com",
+    databaseURL: "https://webapp-59892.firebaseio.com",
+    projectId: "webapp-59892",
+    storageBucket: "webapp-59892.appspot.com",
+    messagingSenderId: "745858206208",
+    appId: "1:745858206208:web:48348b8b92c7a1113966e5",
+    measurementId: "G-ZGRHMWGR9Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase ;
+
+
+
+
+
+
+  
